d9eeffc1e4dc4158faa1f01453cfc9071330cf63
Sentinel2A <86085741+Sentinel2A@users.noreply.github.com>
2021-06-17 21:40:05 -0400

    Add files via upload

---
+++ tf_thing.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+import pip
+
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
